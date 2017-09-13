3abf21ae946753d877a958823621116a43f7cd4d
Peter Moran <petecmo@gmail.com>
2017-09-12 21:18:42 -0400

    Added extra feature extractors.

--- train.py
+++ train.py
@@ -20,46 +20,10 @@ from sklearn.svm import LinearSVC
 from sklearn.utils import shuffle
 
 
-class FeatureVectorBuilder:
-    Sample = TypeVar('Sample')
-    ProcessedSample = TypeVar('ProcessedSample')
-
-    def __init__(self, preprocessor_func: Callable[[Sample], ProcessedSample]):
-        self._preprocessor_func = preprocessor_func
-        self._extractor_funcs = []
-
-    def add_extractor(self, extractor_func: Callable[[ProcessedSample], ndarray]):
-        self._extractor_funcs.append(extractor_func)
-
-    def get_features(self, samples: Sample):
-        # Determine feature vector size
-        extractor_return_len = []
-        for extractor_func in self._extractor_funcs:
-            extractor_return_len.append(len(extractor_func(self._preprocessor_func(samples[0]))))
-        feature_vec_len = np.sum(extractor_return_len)
-
-        # Find the feature vector for every sample
-        feature_vectors = np.zeros(shape=(len(samples), feature_vec_len))
-        for i, sample in enumerate(samples):
-            processed_sample = self._preprocessor_func(sample)
-
-            # Add to the feature vector in chunks from each extractor function
-            start = 0
-            for j, extractor_func in enumerate(self._extractor_funcs):
-                stop = start + extractor_return_len[j]
-                feature_vectors[i, start:stop] = extractor_func(processed_sample)  # ensure features are flattened
-                start = stop
-
-        # Normalize over features (column wise).
-        X_normalized = StandardScaler().fit(feature_vectors).transform(feature_vectors)
-        return X_normalized
-
-
-def extract_hog_features(image: ndarray, n_orient=9, pix_per_cell=8, cell_per_block=2, cspace='RGB',
-                         hog_channels='ALL'):
+def hog_features(image, n_orient=9, pix_per_cell=8, cell_per_block=2, cspace='BGR', hog_channels='ALL'):
     # Apply color conversion
-    if cspace != 'RGB':
-        conversion = getattr(cv2, 'COLOR_RGB2{}'.format(cspace))
+    if cspace != 'BGR':
+        conversion = getattr(cv2, 'COLOR_BGR2{}'.format(cspace))
         feature_image = cv2.cvtColor(image, conversion)
     else:
         feature_image = np.copy(image)
@@ -83,6 +47,70 @@ def extract_hog_features(image: ndarray, n_orient=9, pix_per_cell=8, cell_per_bl
     return np.ravel(hog_features)
 
 
+def bin_spatial(img, size=(32, 32)):
+    features = cv2.resize(img, size).ravel()
+    return features
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256), channels='ALL'):
+    # Select channels to include
+    if channels == 'ALL':
+        channels = range(img.shape[2])
+    else:
+        assert isinstance(channels, Sequence[int]), "`channels` must be a sequence of ints or 'ALL'."
+
+    # Bin each channel and combine
+    histograms = []
+    for channel in channels:
+        histogram, bin_edges = np.histogram(img[:, :, channel], bins=nbins, range=bins_range)
+        histograms.append(histogram)
+    return np.concatenate(histograms)
+
+
+class FeatureVectorBuilder:
+    Sample = TypeVar('Sample')
+    ProcessedSample = TypeVar('ProcessedSample')
+
+    def __init__(self, preprocessor_func: Callable[[Sample], ProcessedSample]):
+        self._preprocessor_func = preprocessor_func
+        self._extractor_funcs = []
+
+    def add_extractor(self, extractor_func: Callable[[ProcessedSample], ndarray]):
+        self._extractor_funcs.append(extractor_func)
+
+    def get_features(self, samples: Sample, verbose=False):
+        if verbose:
+            print('\nExtracting features...')
+            t1 = time.time()
+
+        # Determine feature vector size
+        extractor_return_len = []
+        for extractor_func in self._extractor_funcs:
+            extractor_return_len.append(len(extractor_func(self._preprocessor_func(samples[0]))))
+        feature_vec_len = np.sum(extractor_return_len)
+
+        # Find the feature vector for every sample
+        feature_vectors = np.zeros(shape=(len(samples), feature_vec_len))
+        for i, sample in enumerate(samples):
+            processed_sample = self._preprocessor_func(sample)
+
+            # Add to the feature vector in chunks from each extractor function
+            start = 0
+            for j, extractor_func in enumerate(self._extractor_funcs):
+                stop = start + extractor_return_len[j]
+                feature_vectors[i, start:stop] = extractor_func(processed_sample)  # ensure features are flattened
+                start = stop
+
+        # Normalize over features (column wise).
+        X_normalized = StandardScaler().fit(feature_vectors).transform(feature_vectors)
+
+        if verbose:
+            print('Done (after {:.1f} seconds).'.format(time.time() - t1))
+            print('Feature vector length:', len(X_normalized[0]))
+
+        return X_normalized
+
+
 if __name__ == '__main__':
     # Divide up into files_car and files_notcars
     files_car = glob.glob('./data/vehicles/*/*.png')
@@ -92,23 +120,21 @@ if __name__ == '__main__':
     print('Total number of notcar all_files:', len(files_notcars))
 
     # Reduce the sample size to speed things up
-    sample_size = 500
-    print('Using {} samples each, {} total.'.format(sample_size, sample_size * 2))
+    sample_size = 8500
+    print('Using {} samples each, {} total samples.'.format(sample_size, sample_size * 2))
     files_car = shuffle(files_car)[:sample_size]
     files_notcars = shuffle(files_notcars)[:sample_size]
 
     # Define feature extractor
-    feature_builder = FeatureVectorBuilder(preprocessor_func=lambda file: mpimg.imread(file))
-    feature_builder.add_extractor(lambda img: extract_hog_features(img, cspace='HSV', cell_per_block=3))
+    feature_builder = FeatureVectorBuilder(preprocessor_func=lambda file: cv2.imread(file))
+    feature_builder.add_extractor(lambda img: hog_features(img, cspace='HSV', cell_per_block=3))
+    feature_builder.add_extractor(bin_spatial)
+    feature_builder.add_extractor(color_hist)
 
     # Extract features
     all_files = files_car + files_notcars
-    print('\nExtracting features...')
-    t1 = time.time()
-    X = feature_builder.get_features(all_files)
+    X = feature_builder.get_features(all_files, verbose=True)
     y = np.hstack((np.ones(len(files_car)), np.zeros(len(files_notcars))))
-    print('Done (after {:.1f} seconds).'.format(time.time() - t1))
-    print('Feature vector length:', len(X[0]))
 
     # Split into train/test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
